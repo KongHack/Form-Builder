1ab258c7c7ae2336158ce1670cf35172f8aa4f7c
GameCharmer <admin@gamecharmer.com>
2015-05-18 11:19:17 -0400

    Toggles

--- src/Elements/Toggle.php
+++ src/Elements/Toggle.php
@@ -13,15 +13,81 @@ class Toggle extends BaseElement
 	protected $buttonSet    = array();
 
 	/**
+	 * @param $default
+	 * @return $this
+	 * @throws \Exception
+	 */
+	public function useDefault($default)
+	{
+		switch($default)
+		{
+			case 'y-n':
+				$this->addButton(1,'Yes');
+				$this->addButton(0,'No');
+				break;
+			case 'y-n-na':
+				$this->addButton(1,'Yes');
+				$this->addButton(0,'No');
+				$this->addButton(-1,'N/A');
+				break;
+			case 'a-i':
+				$this->addButton(1,'Active');
+				$this->addButton(0,'Inactive');
+				break;
+			default:
+				throw new \Exception('Invalid default value.');
+				break;
+		}
+
+
+		return $this;
+	}
+
+	/**
+	 * @param        $value
+	 * @param        $caption
+	 * @param string $class
+	 * @return $this
+	 */
+	public function addButton($value, $caption, $class = 'default')
+	{
+		$this->buttonSet[] = array('value'=>$value,'caption'=>$caption,'class'=>$class);
+		return $this;
+	}
+
+	/**
 	 * @return string
+	 * @throws \Exception
 	 */
 	public function render()
 	{
-		$out = '';
+		if(count($this->buttonSet) < 1)
+		{
+			throw new \Exception('No toggles defined');
+		}
 
 
+		$out = '<div class="btn-group" data-toggle="buttons">';
 
+		foreach($this->buttonSet as $button)
+		{
+			$out .= '
+			<label class="btn btn-'.$button['class'];
+			if($this->elementValue == $button['value'])
+			{
+				$out .= ' active';
+			}
+			$out .= '">
+			<input type="radio" name="'.$this->elementName.'" value="'.$button['value'].'"';
+			if($this->elementValue == $button['value'])
+			{
+				$out .= ' checked="checked"';
+			}
+			$out .= '> '.$button['caption'].'</label>';
+
+		}
+
+		$out .= '</div>';
 		return $out;
 	}
-
 }
